fcef7b725c67ec2a2c41fce1b13e616d1b4b6389
kutang <mengyang.Zhang070601@outlook.com>
2026-06-20 15:50:11 +0800

    修改

--- 6-19/text.c
+++ 6-19/text.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
-//单词规律
-//https://leetcode.cn/problems/word-pattern/description/
-
-
 //比赛中的配对次数
 //https://leetcode.cn/problems/count-of-matches-in-tournament/description/
+
 int numberOfMatches(int n) {
     int all = 0;
     int x = n;
