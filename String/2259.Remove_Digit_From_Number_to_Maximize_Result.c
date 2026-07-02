2f58de4b47274eb2bf45cdd3bf48dc0adca7dd29
kutang <mengyang.Zhang070601@outlook.com>
2026-07-02 19:25:30 +0800

    2026.7.1

--- String/2259.Remove_Digit_From_Number_to_Maximize_Result.c
+++ String/2259.Remove_Digit_From_Number_to_Maximize_Result.c
@@ -1,24 +1,71 @@
 #include <stdio.h>
 
+//贪心求解
+char* removeDigit(char* number, char digit) {
+    int len = strlen(number);
+    char* ch = malloc(len + 1);
+    char* pcur = ch;
+    int n = 0;
+    int last = -1;
+    int p = 0;
+
+    while(*number)
+    {
+        if (*number == digit)
+        {
+            last = p;
+            if (((*number < *(number + 1)) || *(number + 1) == '\0') && n == 0)
+            {
+                n++;
+                number++;
+                continue;
+            }
+            else
+            {
+                *pcur = *number;   
+                pcur++;
+            }
+        }
+        else
+        {
+            *pcur = *number;   
+            pcur++;
+        }
+        number++;
+        p++;
+    }
+    if (n == 0)
+    {
+        for (int x = last; x < len - 1; x++)
+        {
+            ch[x] = ch[x + 1];
+        }
+        ch[len - 1] = '\0';
+    }
+
+    *pcur = '\0';
+
+    return ch;
+}
+
+//代码优化
 char* removeDigit(char* number, char digit)
 {
     int len = strlen(number);
     int index = 0;
 
-    // 找删除位置
     for (int i = 0; i < len; i++)
     {
         if (number[i] == digit)
         {
             index = i;
 
-            // 如果后面更大，就优先删这里
             if (i + 1 < len && number[i + 1] > digit)
                 break;
         }
     }
 
-    char* res = (char*)malloc(len); // len-1 + '\0'
+    char* res = (char*)malloc(len); 
     int k = 0;
 
     for (int i = 0; i < len; i++)
