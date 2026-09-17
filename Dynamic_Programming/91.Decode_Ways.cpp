c7ae3fe307be2766915608284e29d39b6855e1d4
kutang <mengyang.Zhang070601@outlook.com>
2026-09-17 23:55:41 +0800

    2026.9.17

--- Dynamic_Programming/91.Decode_Ways.cpp
+++ Dynamic_Programming/91.Decode_Ways.cpp
@@ -1,37 +1,52 @@
 #include <string>
 #include <vector>
 
+//动态规划，定义 dp[i] 为前 i 个在字符解码个数
+// 一个一个判断，先判断这一个是否为0，非0则与前一个构成类别相同
+//然后再判断前一个是否为0，如果非0,则判断和是否大于26,如不大于，则加上前n - 2个字符的解码个数
+//时间复杂度：O(n)
 class Solution 
 {
     public:
         int numDecodings(std::string s) 
         {
-             int n = s.size();
+            if (*s.begin() == '0')
+                return 0;
 
-            std::vector<int> dp(n + 1, 0);
+            if (s.size() == 1)
+                return 1;
+
+            std::vector<int> dp(s.size() + 1);
 
             dp[0] = 1;
+            dp[1] = 1;
 
-            if (s[0] != '0')
+            for (int n = 2; n <= s.size(); n++)
             {
-                dp[1] = 1;
+                if (s[n - 1] != '0')
+                {
+                    dp[n] = dp[n - 1];
+
+                    int n1 = s[n - 1] - '0';
+                    int n2 = (s[n - 2] - '0') * 10;
+
+                    if (n2 == 0)
+                        continue;
+
+                    int sum = n1 + n2;
+
+                    if (sum <= 26)
+                    {
+                        dp[n] += dp[n - 2];
+                    }
+                }
+                else 
+                {
+                    if (s[n - 2] == '1' || s[n - 2] == '2')
+                        dp[n] = dp[n - 2];
+                }
             }
 
-            for (int i = 2; i <= n; i++)
-            {
-                if (s[i - 1] != '0')
-                {
-                    dp[i] += dp[i - 1];
-                }
-
-                int num = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
-
-                if (num >= 10 && num <= 26)
-                {
-                    dp[i] += dp[i - 2];
-                }
-            }
-
-            return dp[n];
+            return dp.back();
         }
 };
